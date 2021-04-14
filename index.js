6e1aed6807e8dad76af8ba67a7901e291409d553
CHUN-Rex Fong <chun-rexfong@CHUN-Rexs-MacBook-Pro.local>
2021-04-14 10:47:50 -0700

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const axios = require('axios');
+const qs = require('qs');
+
+app.get('/', function (req, res) {
+	axios({
+	  method: 'POST',
+	  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+	  data: qs.stringify({
+	    secret: process.env.CAPTCHA_SECRET,
+	    response: req.query.code
+	  }),
+	  url: 'https://www.google.com/recaptcha/api/siteverify',
+	})
+	.then(function (response) {
+		// console.log(response)
+		res.send(response.data)
+	})
+	.catch(function (error) {
+		res.status(500).send(error)
+	});
+})
+ 
+app.listen(process.env.PORT || 3000)
